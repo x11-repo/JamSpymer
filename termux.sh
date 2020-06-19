7d646f8021be4c061e84f78820e99ba7dc2187be
Mikhail Kuznetsov <41520056+mishailovic@users.noreply.github.com>
2020-06-19 11:12:12 +0300

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -3,7 +3,6 @@ pkg update
 pkg upgrade
 pkg install git
 pkg install pip
-pkg install python3-pip
 pkg install python
 pip install -r requirements.txt
 git clone https://github.com/x11-repo/jamspymer.git
