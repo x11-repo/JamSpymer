8d0468484bf46043f2ea02cde4665d7cf5b9c497
Mikhail Kuznetsov <41520056+mishailovic@users.noreply.github.com>
2020-06-19 10:35:45 +0300

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,9 @@
+cd
+pkg update
+pkg upgrade
+pkg install pip
+pkg install python3-pip
+pip install -r requirements.txt
+git clone https://github.com/x11-repo/jamspymer.git
+cd ~/JamSpymer
+python3 JamSpymer.py
