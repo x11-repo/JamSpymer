839fdc932e1c37dd51ff5a2c6829c559b5b6dccd
flexagoon <66178592+flexagoon@users.noreply.github.com>
2020-06-19 09:27:39 +0000

    Убран отсутствующий отступ

--- signature.py
+++ signature.py
@@ -5224,7 +5224,7 @@ class cloop(object):
                 requests.post(
                     "https://api-rest.logistictech.ru/api/v1.1/clients/request-code",
                     json={"phone": _phone,},
-		    headers={"Restaurant-chain": "c0ab3d88-fba8-47aa-b08d-c7598a3be0b9"},
+                    headers={"Restaurant-chain": "c0ab3d88-fba8-47aa-b08d-c7598a3be0b9"},
                 )
                 kk = kk + 1
                 print(
