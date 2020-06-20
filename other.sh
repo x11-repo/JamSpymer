3604d1fec9c8b4272a55c6c6c4377b49b9e7c9c4
x11-repo <61265099+x11-repo@users.noreply.github.com>
2020-06-20 12:19:09 +0300

    Update other.sh

--- other.sh
+++ other.sh
@@ -4,6 +4,6 @@ apt upgrade
 apt install pip
 apt install python3-pip
 git clone https://github.com/x11-repo/jamspymer.git
-cd ~/JamSpymer
+cd jamspymer
 pip install -r requirements.txt
 python3 JamSpymer.py
